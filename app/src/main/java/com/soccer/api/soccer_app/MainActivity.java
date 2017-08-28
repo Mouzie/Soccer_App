ffafe31b14c30d288d5deb372d020cece9e4c462
moses.mathebula16@gmail.com <Mouzie1621>
2017-08-28 15:53:46 +0200

    JSON object created -

--- app/src/main/java/com/soccer/api/soccer_app/MainActivity.java
+++ app/src/main/java/com/soccer/api/soccer_app/MainActivity.java
@@ -1,13 +1,112 @@
 package com.soccer.api.soccer_app;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.soccer.api.soccer_app.com.soccer.api.soccer_app.adapter.SoccerAdapter;
+import com.soccer.api.soccer_app.com.soccer.api.soccer_app.model.Teams;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mTeamRecylerView;
+    private SoccerAdapter mAdapter;
+    private ArrayList<Teams> mTeamsCollections;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        mTeamRecylerView = (RecyclerView)findViewById(R.id.teams_recylcer_view);
+        mTeamRecylerView.setLayoutManager(new LinearLayoutManager(this));
+        mTeamRecylerView.setHasFixedSize(true);
+        mTeamsCollections = new ArrayList<>();
+        mAdapter = new SoccerAdapter(mTeamsCollections, this);
+        mTeamRecylerView.setAdapter(mAdapter);
+    }
+
+    //Class used to fetch data
+    public class FetchData extends AsyncTask<Void, Void, Void>{
+
+        //String used to soccer api uri
+        private String mSoccerAPI;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            //API is consumed using the string located in values/strings.xml file.
+            Uri builtUri = Uri.parse(getString(R.string.football_api));
+            URL url;
+            try{
+                url = new URL(builtUri.toString());
+                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("user-key","1a7000e80c0f423f844dd6e4e6e74418");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if(inputStream == null){
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line + "\n");
+                }
+
+                if(buffer.length() == 0){
+                    return null;
+                }
+
+                mSoccerAPI = buffer.toString();
+                JSONObject jsonOBJ =  new JSONObject(mSoccerAPI);
+
+                Log.v("Response", jsonOBJ.toString());
+
+                JSONArray teamsArray = jsonOBJ.getJSONArray("teams");
+
+                for(int i = 0; i < teamsArray.length(); i++){
+                    Log.v("Team", i + "");
+                    String name;
+                    String coach;
+                    String crestUrl;
+
+                    JSONObject jTeams = (JSONObject) teamsArray.get(i);
+                    jTeams = jTeams.getJSONObject("teams");
+
+                    name = jTeams.getString("name");
+                    coach = jTeams.getString("coach");
+                    //Crest URL to be sorted out soon.
+
+
+                    
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/soccer/api/soccer_app/com/soccer/api/soccer_app/adapter/SoccerAdapter.java
+++ app/src/main/java/com/soccer/api/soccer_app/com/soccer/api/soccer_app/adapter/SoccerAdapter.java
@@ -2,11 +2,14 @@ package com.soccer.api.soccer_app.com.soccer.api.soccer_app.adapter;
 
 import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.soccer.api.soccer_app.R;
 import com.soccer.api.soccer_app.com.soccer.api.soccer_app.model.Teams;
 
@@ -29,17 +32,29 @@ public class SoccerAdapter extends RecyclerView.Adapter<SoccerAdapter.SoccerHold
 
     @Override
     public SoccerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        //The new view will be returned once it's inflated
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.teams_layout, parent, false);
+        return new SoccerHolder(view);
     }
 
     @Override
     public void onBindViewHolder(SoccerHolder holder, int position) {
+        //Getting data from the class Teams
+        Teams teams = mData.get(position);
 
+        //Holder will hold set both team and coach names.
+        holder.setName(teams.getName());
+        holder.setName(teams.getCoach());
+
+        //Glide implementation for images
+        //Will be implemented soon
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if(mData == null)
+            return 0;
+        return mData.size();
     }
 
     public class SoccerHolder extends RecyclerView.ViewHolder{
